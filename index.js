51f20aa0ee227ffe0125a7d8e77244d6d868b034
Rohit Kumar <rohit.kumar@limechat.ai>
2023-06-04 04:43:45 +0530

    Revert "refactor and add sentry to track error" title
    This reverts commit 300a150dec8d64f517b0c1621299e83f4e3c9895. desc

--- index.js
+++ index.js
@@ -1,71 +1,40 @@
-import CodeRunner from './helpers/run.js';
-import dotenv from 'dotenv';
-import rabbit from './helpers/rabbit.js';
-import redisClient from './helpers/redis.js';
-import * as Sentry from "@sentry/node";
+import CodeRunner from './helpers/run.js'
+import dotenv from 'dotenv'
+import rabbit from './helpers/rabbit.js'
+import redisClient from './helpers/redis.js'
 
-dotenv.config();
+dotenv.config()
 
-Sentry.init({
-	dsn: process.env.SENTRY_DSN,
-	tracesSampleRate: 1.0,
-});
+const codeRunner = new CodeRunner()
 
-class Worker {
-	constructor() {
-		this.codeRunner = new CodeRunner();
-		this.queueName = 'MS_TM_Q';
-	}
-
-	async start() {
-		console.log('Starting worker');
-		await this.connectToRabbitMQ();
-		await this.consumeMessages();
-	}
-
-	async connectToRabbitMQ() {
-		await rabbit.connect();
-		await rabbit.channel.assertQueue(this.queueName);
-	}
-
-	async consumeMessages() {
-		await rabbit.consume(this.queueName, async (msg, message) => {
-			console.log('Consuming message');
-			if (message === null) {
-				console.log('Message is null');
-				return;
-			}
-
-			await this.processMessage(msg, message);
-		});
-	}
-
-	async processMessage(msg, message) {
+const runWorker = async () => {
+	console.log('Starting worker')
+	await rabbit.connect()
+	const { channel } = rabbit
+	await rabbit.channel.assertQueue('MS_TM_Q')
+	await rabbit.consume('MS_TM_Q', async (msg, message) => {
+		console.log('Consuming message')
 		try {
-			const { code, lang, id } = message;
-			const { stdout, stderr } = await this.codeRunner.runCode(code, lang);
-			const result = stdout ? stdout : stderr;
-			await this.storeResult(id, result);
-		} catch (error) {
-			console.log('Error', error);
-			if (process.env.NODE_ENV === 'production') {
-				Sentry.captureException(error);
+			if (message === null) {
+				console.log('Message is null')
+				return
 			}
+			const { code, lang, id } = message
+			const { stdout, stderr } = await codeRunner.runCode(code, lang)
+			const result = stdout ? stdout : stderr
+			const output = {
+				result,
+				status: 'completed'
+			}
+			console.log('Output', output)
+			await redisClient.set(id, JSON.stringify(output))
+		} catch (error) {
+			console.log('Error', error)
 		} finally {
-			rabbit.channel.ack(msg);
-			console.log('Message acknowledged');
+			channel.ack(msg)
+			console.log('Message acknowledged')
 		}
-	}
-
-	async storeResult(id, result) {
-		const output = {
-			result,
-			status: 'completed'
-		};
-		console.log('Output', output);
-		await redisClient.set(id, JSON.stringify(output));
-	}
+	})
 }
 
-const worker = new Worker();
-worker.start();
+runWorker()
