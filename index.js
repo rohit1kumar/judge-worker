f80f4fbafa637961fb8ce5c89d201f7e9b368ca9
Rohit Kumar <rohit.kumar@limechat.ai>
2023-06-04 11:32:17 +0530

    add error handler in runner

--- index.js
+++ index.js
@@ -13,36 +13,43 @@ Sentry.init({
 const codeRunner = new CodeRunner()
 
 const runWorker = async () => {
-	console.log('Starting worker')
-	await rabbit.connect()
-	const { channel } = rabbit
-	await rabbit.channel.assertQueue('MS_TM_Q')
-	await rabbit.consume('MS_TM_Q', async (msg, message) => {
-		console.log('Consuming message')
-		try {
-			if (message === null) {
-				console.log('Message is null')
-				return
+	try {
+		console.log('Starting worker')
+		await rabbit.connect()
+		const { channel } = rabbit
+		await rabbit.channel.assertQueue('MS_TM_Q')
+		await rabbit.consume('MS_TM_Q', async (msg, message) => {
+			console.log('Consuming message')
+			try {
+				if (message === null) {
+					console.log('Message is null')
+					return
+				}
+				const { code, lang, id } = message
+				const { stdout, stderr } = await codeRunner.runCode(code, lang)
+				const result = stdout ? stdout : stderr
+				const output = {
+					result,
+					status: 'completed'
+				}
+				console.log('Output', output)
+				await redisClient.set(id, JSON.stringify(output))
+			} catch (error) {
+				console.log('Error', error)
+				if (process.env.NODE_ENV === 'production') {
+					Sentry.captureException(error)
+				}
+			} finally {
+				channel.ack(msg)
+				console.log('Message acknowledged')
 			}
-			const { code, lang, id } = message
-			const { stdout, stderr } = await codeRunner.runCode(code, lang)
-			const result = stdout ? stdout : stderr
-			const output = {
-				result,
-				status: 'completed'
-			}
-			console.log('Output', output)
-			await redisClient.set(id, JSON.stringify(output))
-		} catch (error) {
-			console.log('Error', error)
-			if (process.env.NODE_ENV === 'production') {
-				Sentry.captureException(error)
-			}
-		} finally {
-			channel.ack(msg)
-			console.log('Message acknowledged')
+		})
+	} catch (error) {
+		console.log('Error', error)
+		if (process.env.NODE_ENV === 'production') {
+			Sentry.captureException(error)
 		}
-	})
+	}
 }
 
 runWorker()
